df46cdbdf3302a97212d1cd2dd52041ac727d165
moffin <moffin@moffin-K53SJ.(none)>
2013-10-20 18:25:25 +0300

    media roots

--- flactovk/settings.py
+++ flactovk/settings.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/moffin/Desktop/flactovk.com/media/'
+MEDIA_ROOT = '/var/www/flacshtok.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/moffin/Desktop/flactovk.com/static/'
+STATIC_ROOT = '/var/www/flacshtok.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
